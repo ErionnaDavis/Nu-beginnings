b2adbee8d1b95dd3e037d107a7a37ac5fb8c04a2
Erionna Davis <erionnadavis15@gmail.com>
2017-08-09 14:42:19 -0500

    changes

--- src/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var swiper = new Swiper('.swiper-container', {
-        pagination:'.swiper-pagination',
-        slidesPerView: 3,
-        paginationClickable: true,
-        spaceBetween: 30
-    });
-}); 
